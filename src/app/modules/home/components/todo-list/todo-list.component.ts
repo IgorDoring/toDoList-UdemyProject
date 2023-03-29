f4d30184c8638efa7749e7dfa1932fe8fea28441
IgorDoring <idoring@tutanota.com>
2023-03-29 00:38:51 -0300

    show Mocking complete

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
 
+export class TodoListComponent {
+  public todoList: Array<TaskList> = [];
+
+  mock(){
+    this.todoList.push({task: "task ", checked: false});
+    this.todoList.push({task: "task a", checked: false});
+    this.todoList.push({task: "task b", checked: false});
+    this.todoList.push({task: "task c", checked: false});
+    this.todoList.push({task: "task d", checked: true});
+    this.todoList.push({task: "task e", checked: true});
+    console.log(this.todoList.length);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void{
+    this.mock();
+  }
+}
+
+export interface TaskList {
+  task: string, 
+  checked: boolean
 }
